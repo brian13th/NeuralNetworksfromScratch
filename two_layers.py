c2d438e033450a65ec54475a2070963cdef9b8a3
brian13th <brian13th13@gmail.com>
2020-05-02 00:22:54 +0300

    changed inputs array

--- two_layers.py
+++ two_layers.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# two layer neurons, the first one feeds the second
+# two layer neurons, the first one feeds the second (changed the inputs to be the same as Sentdex series)
 
 inputs = [[1, 2, 3, 2.5],
           [2.0, 5.0, -1.0, 2.0],

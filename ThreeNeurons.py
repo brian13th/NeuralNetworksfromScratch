cdeb51f28181e79a500665b10f767f9ee9fb3ce8
brian13th <brian13th13@gmail.com>
2020-05-01 22:59:51 +0300

    some pep8 compliance

--- ThreeNeurons.py
+++ ThreeNeurons.py
@@ -8,7 +8,7 @@ weights = [[0.8, 1, -1, 0.5],
            [0.2, 0.9, 1, -0.2],
            [2, 1.5, -1, -0.5]]
 
-biases = [1, -1, 2] #three biases as we have three neuros and each one has it's own output gravity
+biases = [1, -1, 2]  # three biases as we have three neurons and each one has it's own output gravity
 
 outputs = np.dot(weights, inputs) + biases
 print(outputs)
